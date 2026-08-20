f6fe33259d24f4580d2194141e75b64739182b2e
pivotops <craftstreams@gmail.com>
2026-08-20 11:17:26 +0100

    fix: make SaaS Browser badge responsive on mobile viewports

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -738,9 +738,18 @@ export default function HomeClient() {
             </div>
           </div>
         </div>
-            <div className="flex justify-center pt-2">
-              <a href="https://saasbrowser.com/en/saas/1613929/pivotops" target="_blank" rel="noopener">
-                <img src="https://static-files.saasbrowser.com/saas-browser-badge-14.svg" alt="PivotOps - SaaS discovery platform" width="200" />
+            <div className="flex justify-center pt-2 px-4 w-full">
+              <a
+                href="https://saasbrowser.com/en/saas/1613929/pivotops"
+                target="_blank"
+                rel="noopener"
+                className="inline-block max-w-full"
+              >
+                <img
+                  src="https://static-files.saasbrowser.com/saas-browser-badge-14.svg"
+                  alt="PivotOps - SaaS discovery platform"
+                  className="w-[200px] max-w-full h-auto block"
+                />
               </a>
             </div>
       </footer>
